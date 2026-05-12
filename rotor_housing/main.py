5bc68e2942039e5ec70c520eaee9763a27263ac6
hnineieitun <ms.hnineieitun@gmail.com>
2026-05-12 20:36:32 +0200

    Add url and stuff

--- rotor_housing/main.py
+++ rotor_housing/main.py
@@ -1,18 +1,66 @@
 from fastapi import FastAPI
+import rotor
+import requests
+from pydantic import BaseModel
 
 app = FastAPI()
 
-gloabl_states = {}
+reflector = {
+    "from": [],
+    "to": []
+}
 
-@app.get("/")
-def read(q: str, configuration: str | None = None):
-    return {"q": q, "configuration": str}
+class States(BaseModel):
+    reflector: str 
+    r1_position: str
+    r2_position: str
+    r3_position: str
 
+global_states = {
+    "reflector": None,
+    "r1_position": None,
+    "r2_position": None,
+    "r3_position": None,
+}
+
+# @app.get("/")
+# def read(q: str, configuration: str | None = None):
+#     return {"q": q, "configuration": str}
+
+# to rotor
+@app.post("/send")
+def send_key(k: str):
+    # rotors call 321
+    response = requests.get('http://rotor/forward', data={
+        "key": k,
+        "state": gloabl_states
+    }).json()
+
+    # todo: reflector call
+    character = reflect(response["char"])
+
+    # todo: rotors call 321
+    response = requests.get('http://rotor/backward', data={
+        "key": character,
+        "state": gloabl_states
+    }).json()
+
+    gloabl_states = response["state"]
+
+def reflect(input_char):
+    pass
+    # todo: do reflection stuff here
+
+# set states
 @app.post("/states")
-def set_state(states):
+def set_state(states: States):
     print(states)
-    gloabl_states = states
+    global global_states
+    
+    global_states = states
+    return True
 
+# get states
 @app.get("/states")
 def states():
-    return gloabl_states
+    return global_states
--- rotor_housing/rotor.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def ask(q):
-    return requests.get('http://rotor', params={
-        q: q
-    }).json()
